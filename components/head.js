95d42f1a9c11be12c82435b471c19c0c4ff4ff60
Florent Segouin <florent@segouin.me>
2020-05-18 18:59:48 +0100

    Hello 2020

--- components/head.js
+++ components/head.js
@@ -1,30 +1,28 @@
 import NextHead from 'next/head'
 import { string } from 'prop-types'
 
-const defaultTitle = 'segouin.xyz'
-const defaultDescription = 'Florent Segouin'
 const defaultOGURL = 'https://segouin.xyz'
-const defaultOGImage = 'https://segouin.xyz/static/thumb.png'
+const defaultOGImage = 'https://segouin.xyz/assets/images/thumb.jpg'
+const defaultTitle = 'segouin.xyz'
+const defaultDescription = 'Who cares?'
 const defaultType = 'website'
 
-const Head = props => (
+const Head = (props) => (
   <NextHead>
-    <meta charSet="UTF-8" />
-    <title>{props.title || defaultTitle}</title>
-    <meta name="description" content={props.description || defaultDescription} />
+    <title>{props.title}</title>
+    <meta charSet="utf-8" />
+    <meta
+      name="description"
+      content={props.description || defaultDescription}
+    />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
-    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" as="style" onload="this.rel='stylesheet'"/>
-    <noscript><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" /></noscript> 
-    <link rel="preload" href="https://fonts.googleapis.com/css?family=Work+Sans:400,500,600" as="style" onload="this.rel='stylesheet'" />
-    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:400,500,600" /></noscript>
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || defaultTitle} />
     <meta property="og:type" content={props.type || defaultType} />
-    <meta property="og:description" content={props.description || defaultDescription} />
+    <meta
+      property="og:description"
+      content={props.description || defaultDescription}
+    />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../public/assets/styles/fira_code.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
